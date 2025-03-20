e14b921b6be7a8243d188bf29dd6f0d8b8ab7e3c
Felipe Alencar <gia.comini@hotmail.com>
2025-03-20 17:17:26 -0300

    Logica para gerar arquivo excel criada

--- src/main/java/com/studyings3/filemanagers3/serviceImpl/GenerateFileServiceImpl.java
+++ src/main/java/com/studyings3/filemanagers3/serviceImpl/GenerateFileServiceImpl.java
@@ -1,16 +1,71 @@
 package com.studyings3.filemanagers3.serviceImpl;
 
 import com.studyings3.filemanagers3.model.response.GenerateFileResponse;
+import com.studyings3.filemanagers3.model.response.PokemonData;
 import com.studyings3.filemanagers3.service.GenerateFileService;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class GenerateFileServiceImpl implements GenerateFileService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${poke.api.url}")
+    private String url;
     @Override
     public ResponseEntity<GenerateFileResponse> generateFile() {
         System.out.println("Teste 1");
+
+        ResponseEntity<PokemonData> response = getPokemonData();
+        var pokeData = response.getBody();
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Pokemon Data");
+
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("ID");
+        headerRow.createCell(1).setCellValue("Name");
+
+        Row dataRow = sheet.createRow(1);
+
+        dataRow.createCell(0).setCellValue(pokeData.id);
+        dataRow.createCell(1).setCellValue(pokeData.name);
+
+        try (FileOutputStream fileOut = new FileOutputStream("pokemon_data.xlsx")) {
+            workbook.write(fileOut);
+            System.out.println("Excel file created successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    public ResponseEntity<PokemonData> getPokemonData(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange(
+                url+"/pokemon/ditto",
+                HttpMethod.GET,
+                entity,
+                PokemonData.class
+        );
     }
 }
